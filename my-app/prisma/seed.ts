7054cc4300fe0b40ed850e1a409ce74a2a09df26
ElizabethT7 <tymofieievai@gmail.com>
2024-12-14 15:58:50 +0300

    fix: fix game player model

--- my-app/prisma/seed.ts
+++ my-app/prisma/seed.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({
+  /*const user = await prisma.user.create({
     data: { login: 'user', passwordHash: 'asd;lff', rating: 100}
   })
   const user2 = await prisma.user.create({
     data: { login: 'user2', passwordHash: 'asd;lff', rating: 130}
   })
-  /*await prisma.game.create({
+  await prisma.game.create({
     data: {
       field: Array(9).fill(null),
       status: 'idle',
