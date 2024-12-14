9ef6d64fd03191820f6c8fbf03c9977003a8a993
ElizabethT7 <tymofieievai@gmail.com>
2024-12-14 17:13:20 +0300

    fix: fix game create logic

--- my-app/src/entities/game/repositories/game.ts
+++ my-app/src/entities/game/repositories/game.ts
@@ -1,19 +1,20 @@
-import { Game, Prisma, User } from '@prisma/client';
+import { Game, Prisma, User, GamePlayer } from '@prisma/client';
 import { GameEntity, GameIdleEntity, GameOverEntity, PlayerEntity } from '../domain';
 import { prisma } from '@/shared/lib/db';
 import { z } from 'zod';
-import { removePassword } from '@/shared/lib/password';
 import { GameId } from '@/common/ids';
 
 const fieldSchema = z.array(z.union([z.string(), z.null()]));
 
+const gameInclude = { winner: { include: { user: true } }, players: { include: { user: true } } };
+
 function dbGameToGameEntity(
   game: Game & {
-    players: User[];
-    winner?: User | null;
+    players: Array<GamePlayer & { user: User }>;
+    winner?: (GamePlayer & { user: User }) | null;
   }
 ): GameEntity {
-  const players = game.players.map(removePassword);
+  const players = game.players.sort((a, b) => a.index - b.index).map(dbPlayerToPlayer);
   switch (game.status) {
     case 'idle': {
       const [creator] = players;
@@ -45,7 +46,7 @@ function dbGameToGameEntity(
         players: players,
         status: game.status,
         field: fieldSchema.parse(game.field),
-        winner: removePassword(game.winner),
+        winner: dbPlayerToPlayer(game.winner),
       } satisfies GameOverEntity;
     }
   }
@@ -54,10 +55,7 @@ function dbGameToGameEntity(
 async function gamesList(where?: Prisma.GameWhereInput): Promise<GameEntity[]> {
   const games = await prisma.game.findMany({
     where,
-    include: {
-      winner: true,
-      players: true,
-    },
+    include: gameInclude,
   });
 
   return games.map(dbGameToGameEntity);
@@ -66,10 +64,7 @@ async function gamesList(where?: Prisma.GameWhereInput): Promise<GameEntity[]> {
 async function getGame(where?: Prisma.GameWhereInput) {
   const game = await prisma.game.findFirst({
     where,
-    include: {
-      winner: true,
-      players: true,
-    },
+    include: gameInclude,
   });
 
   if (game) {
@@ -85,16 +80,14 @@ async function startGame(gameId: GameId, player: PlayerEntity) {
       where: { id: gameId },
       data: {
         players: {
-          connect: {
-            id: player.id,
+          create: {
+            index: 1,
+            userId: player.id,
           },
         },
         status: 'inProgress',
       },
-      include: {
-        winner: true,
-        players: true,
-      },
+      include: gameInclude,
     })
   );
 }
@@ -106,16 +99,24 @@ async function createGame(game: GameIdleEntity): Promise<GameEntity> {
       id: game.id,
       field: game.field,
       players: {
-        connect: { id: game.creator.id },
+        create: {
+          index: 0,
+          userId: game.creator.id,
+        },
       },
     },
-    include: {
-      winner: true,
-      players: true,
-    },
+    include: gameInclude,
   });
 
   return dbGameToGameEntity(createdGame);
 }
+
+export const dbPlayerToPlayer = (db: GamePlayer & { user: User }): PlayerEntity => {
+  return {
+    id: db.user.id,
+    login: db.user.login,
+    rating: db.user.rating,
+  };
+};
 
 export const gameRepository = { gamesList, createGame, getGame, startGame };
